e98e7f708875cefec45a16939e61b080b5a669bd
Jorge Mauricio <jorge.ernesto.mauricio@gmail.com>
2020-08-11 11:50:01 -0500

    modificar path de ejecución

--- automatico.sh
+++ automatico.sh
@@ -6,7 +6,7 @@ today="$(date '+%Y-%m-%d')"
 echo $today
 
 # change to work directory
-cd /home/jorge/Documents/Research/get_modis_files_from_nada/
+cd /home/jorge/Documents/Research/get_modis_files_from_nasa/
 
 # execute algoritmo.py
-/home/jorge/anaconda3/bin/python3.7 /home/jorge/Documents/Research/get_modis_files_from_nada/algoritmo.py
+/home/jorge/anaconda3/bin/python3.7 /home/jorge/Documents/Research/get_modis_files_from_nasa/algoritmo.py
